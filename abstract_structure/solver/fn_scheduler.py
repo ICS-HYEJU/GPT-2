083ac2fe7b21b4ef4179f1d0dc1766744f98c1fe
hjchoi <juhye987@naver.com>
2024-07-12 14:00:39 +0900

    [Complete] GPT Scheduler

--- abstract_structure/solver/fn_scheduler.py
+++ abstract_structure/solver/fn_scheduler.py
@@ -4,6 +4,6 @@ def build_scheduler(cfg, optimizer):
     if cfg.scheduler['name'] == 'LambdaLR':
         scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer,
                                                       lr_lambda=cfg.scheduler['lr_lambda'])
-        return scheduler
     else:
         raise NotImplementedError('{} is not Implemeted'.format(cfg['scheduler']['name']))
+    return scheduler
